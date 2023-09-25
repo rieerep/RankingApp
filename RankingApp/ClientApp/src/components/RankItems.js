f2d6d91da4207e9e9104d5917d5182ef46364c71
rieerep <114030611+rieerep@users.noreply.github.com>
2023-09-26 00:21:27 +0200

    first commit

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,27 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+
+	// items kommer från server-side, setItems gäller react-komponenten som kan ändra items state.
+	const [items, setItems] = useState([]);
+	const dataType = 1;
+
+	useEffect(() => {
+		fetch(`item/${dataType}`)
+			.then((results) => {
+				return results.json();
+			})
+			.then(data => {
+				setItems(data);
+			})
+	},[])
+
+	return (
+		<main>
+			{
+				(items != null) ? items.map((item) => <h3>{item.title}</h3>): <div>Loading...</div>
+			}
+		</main>
+	)
+}
+export default RankItems;
